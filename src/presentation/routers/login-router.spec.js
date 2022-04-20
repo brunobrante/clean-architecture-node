5254825e01ec391eac602f44d9575f6a75f52dad
Bruno Brante <brunobrante@hotmail.com>
2022-04-20 15:00:56 -0300

    ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -21,6 +21,7 @@ const makeSut = () => {
 }
 
 describe('Login Router', () => {
+
 	test('Should return 400 if no email is provided', () => {
 		const { sut } = makeSut();
 		const httpRequest = {
@@ -31,7 +32,7 @@ describe('Login Router', () => {
 		const httpResponse = sut.route(httpRequest);
 		expect(httpResponse.statusCode).toBe(400);
 		expect(httpResponse.body).toEqual(new MissingParamError('email'));
-	})
+	});
 
 	test('Should return 400 if no password is provided', () => {
 		const { sut } = makeSut();
@@ -43,14 +44,13 @@ describe('Login Router', () => {
 		const httpResponse = sut.route(httpRequest);
 		expect(httpResponse.statusCode).toBe(400);
 		expect(httpResponse.body).toEqual(new MissingParamError('password'));
-	})
+	});
 
 	test('Should return 500 if no httpRequest is provided', () => {
 		const { sut } = makeSut();
 		const httpResponse = sut.route();
 		expect(httpResponse.statusCode).toBe(500);
-	})
-
+	});
 
 	test('Should return 500 if httpRequest has no body', () => {
 		const { sut } = makeSut();
@@ -70,8 +70,7 @@ describe('Login Router', () => {
 		sut.route(httpRequest);
 		expect(authUseCaseSpy.email).toBe(httpRequest.body.email);
 		expect(authUseCaseSpy.password).toBe(httpRequest.body.password);
-	})
-
+	});
 
 	test('Should return 401 when invalid credentials are provided', () => {
 		const { sut } = makeSut();
@@ -84,5 +83,32 @@ describe('Login Router', () => {
 		const httpResponse = sut.route(httpRequest);
 		expect(httpResponse.statusCode).toBe(401);
 		expect(httpResponse.body).toEqual(new UnauthorizedError());
+	});
+
+	test('Should return 500 if no AuthUseCase is provided', () => {
+		const sut = new LoginRouter();
+		const httpRequest = {
+			body: {
+				email: 'invalid_email@mail.com',
+				password: 'invalid_password'
+			}
+		};
+		const httpResponse = sut.route(httpRequest);
+		expect(httpResponse.statusCode).toBe(500);
+	});
+
+
+	test('Should return 500 if AuthUseCase has no auth method', () => {
+		class AuthUseCaseSpy { }
+		const authUseCaseSpy = new AuthUseCaseSpy()
+		const sut = new LoginRouter(authUseCaseSpy);
+		const httpRequest = {
+			body: {
+				email: 'invalid_email@mail.com',
+				password: 'invalid_password'
+			}
+		};
+		const httpResponse = sut.route(httpRequest);
+		expect(httpResponse.statusCode).toBe(500);
 	})
 })
