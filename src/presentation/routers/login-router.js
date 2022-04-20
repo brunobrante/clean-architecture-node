86338011f0a715a8d0e0b4a36fa8d53abe66b55a
Bruno Brante <brunobrante@hotmail.com>
2022-04-20 14:49:15 -0300

    Add ; on code

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -3,25 +3,25 @@ const HttpResponse = require('../helpers/http-response');
 module.exports = class LoginRouter {
 
 	constructor(authUseCase) {
-		this.authUseCase = authUseCase
+		this.authUseCase = authUseCase;
 	}
 
 	route(httpRequest) {
 		if (!httpRequest || !httpRequest.body) {
-			return HttpResponse.serverError()
+			return HttpResponse.serverError();
 		}
 		const { email, password } = httpRequest.body;
 		if (!email) {
-			return HttpResponse.badRequest('email')
+			return HttpResponse.badRequest('email');
 		}
 
 		if (!password) {
-			return HttpResponse.badRequest('password')
+			return HttpResponse.badRequest('password');
 		}
 
-		this.authUseCase.auth(email, password)
+		this.authUseCase.auth(email, password);
 		return {
 			statusCode: 401
-		}
+		};
 	}
 }
