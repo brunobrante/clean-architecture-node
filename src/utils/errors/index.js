5346e07ce239645a7ad42de637c97e4e510d7c77
Bruno Brante <brunobrante@hotmail.com>
2022-04-20 16:49:39 -0300

    move generic errors to utils layer

---
+++ src/utils/errors/index.js
@@ -0,0 +1,7 @@
+const MissingParamError = require('./missign-param-error');
+const InvalidParamError = require('./invalid-param-error');
+
+module.exports = {
+	MissingParamError,
+	InvalidParamError,
+}
