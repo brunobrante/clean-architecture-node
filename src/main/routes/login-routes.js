4245329c12386b71861cb87c7d91c4e04434a1bf
Bruno Brante <brunobrante@hotmail.com>
2022-04-21 12:01:18 -0300

    move loginRouter composition to its own file

--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,4 +1,4 @@
-const loginRouter = require('../composers/login-router-composer');
+const loginRouter = require('../composers/login-router-composer')
 
 module.exports = router => {
 	router.post('/login', loginRouter)
